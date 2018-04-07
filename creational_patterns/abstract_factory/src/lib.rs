172766c92748d64ed71ebfa4ed542fbe17efafa2
Robert Turner Jr <georob23@gmail.com>
2018-04-06 20:17:14 -0600

    Create library files. Add main.rs as a driver for expeimentation.

--- creational_patterns/abstract_factory/src/lib.rs
+++ creational_patterns/abstract_factory/src/lib.rs
@@ -1,3 +1,14 @@
+pub mod map_site;  // trait to encompass walls and doors
+
+pub mod wall;      // "class" to represent a Wall
+
+pub mod wall;      // "class" to represent a doors
+
+pub fn cheese() {
+    // silly function to test that main can see this library
+    println!("Cheese sure is good!");
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
